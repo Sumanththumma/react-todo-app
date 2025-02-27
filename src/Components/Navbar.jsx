6bc585ac2cd5e8427fd6d8bbee19d984dbb29c5e
Sumanththumma <tsumanth3@gmail.com>
2025-02-27 20:51:21 +0530

    navbar changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
    
   return (
     <>
-        <div className='flex bg-black text-white justify-between px-20 py-5'>
+        <div className='flex bg-black text-white justify-between px-20 py-5 f'>
             <h1>=</h1>
             <h1>Todo App</h1>
             <h1>account</h1>
